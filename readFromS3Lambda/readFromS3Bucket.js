f84c0df20b67cc0e2c9289cce912cf15b1b0021d
tjtln <tjtln@iastate.edu>
2024-03-28 04:47:35 -0500

    updated with batches

--- readFromS3Lambda/readFromS3Bucket.js
+++ readFromS3Lambda/readFromS3Bucket.js
@@ -1,14 +1,14 @@
-import aws from 'aws-sdk';
+import { S3 } from '@aws-sdk/client-s3';
 
 export const handler = async(event) => {
-    const s3 = new aws.S3();
+    const s3 = new S3();
     var data;
     const params = {
         Bucket: "mlbdatabucket",
         Key: "mlbDataForFirstInning.json"
       };
     try {
-        const response = await s3.getObject(params).promise();
+        const response = await s3.getObject(params);
         data = response.Body.toString('utf-8');
         console.log('Response: ', response);
     } catch (err) {
